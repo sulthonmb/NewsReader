f091006305c60d20fb9105ed07170e0cb3d77d70
Sulthon <sulthonmb@gmail.com>
2023-04-28 05:52:06 +0700

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Sulthon on 28/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
