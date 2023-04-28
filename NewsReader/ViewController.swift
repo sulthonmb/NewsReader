ee8bb8d2146ee079ae2d166d5e410e1fe60f3c42
Sulthon <sulthonmb@gmail.com>
2023-04-28 07:27:23 +0700

    Add project

--- NewsReader/ViewController.swift
+++ NewsReader/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        emailTextLabel.text = "Surel"
+        emailTextField.placeholder = "surel@pengguna.com"
+        passwordTextLabel.text = "Kata Sandi"
+        passwordTextField.placeholder = "Rahasia"
+        loginButton.setTitle("Masuk", for: UIControl.State.normal)
+        
+        
     }
 
-
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        print("Email: \(emailTextField.text ?? "-")")
+    }
 }
 
